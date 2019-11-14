38225ba515fd6e90fcd6090688c92868fa9e219e
Nacho 2 <ignacio.abadie@wolox.com.ar>
2019-11-14 16:31:50 -0300

    "Kickoff!"

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+
+import App from './App';
+
+AppRegistry.registerComponent('native_modules_demo', () => App);
---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+const Routes = {
+  App: 'App',
+  Auth: 'Auth',
+  Home: 'Home',
+  InitialLoading: 'InitialLoading',
+  Login: 'Login',
+};
+
+export default Routes;
