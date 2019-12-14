73e644d758fc114417f873a60aeca746edab954b
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2019-12-14 19:00:16 -0300

    Finished cropping

--- ios/ScreenshotHelperModuleManager.swift
+++ ios/ScreenshotHelperModuleManager.swift
@@ -22,17 +22,23 @@ class ScreenshotHelperModule: NSObject {
     DispatchQueue.main.async {
       let layer = UIApplication.shared.keyWindow!.layer
       let scale = UIScreen.main.scale
-      let size = CGSize(width: 20, height: 30)
 
       // Creates UIImage of same size as view
-      UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale)
+      UIGraphicsBeginImageContextWithOptions(layer.frame.size, true, scale)
       layer.render(in: UIGraphicsGetCurrentContext()!)
-      
+
       let screenshot = UIGraphicsGetImageFromCurrentImageContext()
       UIGraphicsEndImageContext()
       
+      // Crop Screenshot
+      let horizontalField = x
+      let verticalField = y
+      let crop = CGRect(x: (250 * Int(truncating: verticalField)), y: 127 + (360 * Int(truncating: horizontalField)), width: 250, height: 360)
+      let cgImage = screenshot?.cgImage?.cropping(to: crop)
+      let croppedImage: UIImage = UIImage(cgImage: cgImage!)
+      
       // THIS IS TO SAVE SCREENSHOT TO PHOTOS
-      UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+      UIImageWriteToSavedPhotosAlbum(croppedImage, nil, nil, nil)
     }
     successCallback([true])
   }
