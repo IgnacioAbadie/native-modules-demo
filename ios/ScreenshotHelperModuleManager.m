04238723a3a9274149fa0aaed0c29dae0c5c5f4e
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2019-12-14 17:49:58 -0300

    Taking full screenshot on iOS too. Missing use x and y params

--- ios/ScreenshotHelperModuleManager.m
+++ ios/ScreenshotHelperModuleManager.m
@@ -2,5 +2,8 @@
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(ScreenshotHelperModule, NSObject)
-RCT_EXTERN_METHOD(takeScreenshot: (RCTResponseSenderBlock *)successCallback)
+
+RCT_EXTERN_METHOD(takeScreenshot:(nonnull NSNumber *)x y:(nonnull NSNumber *)y successCallback:(RCTResponseSenderBlock *)successCallback)
+
 @end
+
--- ios/ScreenshotHelperModuleManager.swift
+++ ios/ScreenshotHelperModuleManager.swift
@@ -11,13 +11,29 @@ import UIKit
 
 @objc(ScreenshotHelperModule)
 class ScreenshotHelperModule: NSObject {
+  
   @objc
   static func requiresMainQueueSetup() -> Bool {
     return true
   }
-  @objc (takeScreenshot:)
-  func takeScreenshot(_ successCallback: RCTResponseSenderBlock) -> Void {
-    
+  
+  @objc (takeScreenshot:y:successCallback:)
+  func takeScreenshot(_ x: NSNumber, y: NSNumber, successCallback: RCTResponseSenderBlock) -> Void {
+    DispatchQueue.main.async {
+      let layer = UIApplication.shared.keyWindow!.layer
+      let scale = UIScreen.main.scale
+      let size = CGSize(width: 20, height: 30)
+
+      // Creates UIImage of same size as view
+      UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale)
+      layer.render(in: UIGraphicsGetCurrentContext()!)
+      
+      let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+      
+      // THIS IS TO SAVE SCREENSHOT TO PHOTOS
+      UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+    }
     successCallback([true])
   }
 }
